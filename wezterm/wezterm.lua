a67f36b57cc2e55a22dc92d57244c9d7d5ff8d8d
tomoyanakano <tomo1999724@gmail.com>
2024-03-19 02:17:31 +0900

    :wrench: Update config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,33 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+-- colors
+config.color_scheme = "nord"
+config.window_background_opacity = 0.8
+
+-- font
+config.font = wezterm.font("0xProto")
+config.font_size = 13.0
+
+-- keybind
+config.disable_default_key_bindings = true
+local keybinds = require("keybinds")
+config.keys = keybinds.keys
+config.key_tables = keybinds.key_tables
+
+config.leader = { key = "q", mods = "CTRL", timeout_milliseconds = 1000 }
+
+-- window
+config.window_close_confirmation = "AlwaysPrompt"
+
+-- and finally, return the configuration to wezterm
+return config
