f2be4173a281e5a54c1dcd5e5ffb6ddc4998e81d
tomoyanakano <tomo1999724@gmail.com>
2025-08-05 21:19:19 +0900

    :wrench: Update config

---
+++ link.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+files_and_paths=(
+  ".config/karabiner:~/.config/karabiner"
+  ".config/wezterm:~/.config/wezterm"
+  ".config/yabai:~/.config/yabai"
+  ".config/skhd:~/.config/skhd"
+  ".config/starship.toml:~/.config/starship.toml"
+)
+
+create_symlink() {
+  local source_file=$(realpath "$1")
+  local destination_path="$2"
+  
+  # チルダを展開
+  destination_path="${destination_path/#\~/$HOME}"
+  
+  # 宛先の親ディレクトリが存在しない場合は作成
+  local destination_dir=$(dirname "$destination_path")
+  mkdir -p "$destination_dir"
+  
+  local backup_file="${destination_path}.bak"
+  
+  if [ -e "$destination_path" ]; then
+    mv "$destination_path" "$backup_file"
+    echo "既存のファイル/ディレクトリを ${backup_file} にバックアップしました"
+  fi
+  
+  ln -s "$source_file" "$destination_path"
+  echo "シンボリックリンクを作成しました: $source_file -> $destination_path"
+}
+
+for entry in "${files_and_paths[@]}"; do
+  IFS=":" read -r source_file destination_path <<< "$entry"
+  create_symlink "$source_file" "$destination_path"
+done
